d40c10db4a00efc1ce39767bc9e3205b15d4c66b
lieza2019 <megafeps@eos.ocn.ne.jp>
2023-05-20 20:14:41 +0900

    Still even in the stage of arguement of symbol table management.

--- tyinf.hs
+++ tyinf.hs
@@ -198,7 +198,30 @@ sym_search symtbl cat ident =
                                                        Nothing -> sym_search (sym_update symtbl cat sym_tbl') cat ident
                                                     )
   )
-
+sym_search' :: Sym_tbl -> String -> Maybe (Sym_attrib, (Sym_tbl, Sym_tbl))
+sym_search' sym_tbl ident =
+  ras_trace "in sym_search" (
+  let walk syms ident =
+        case syms of
+          Sym_empty -> Nothing
+          Sym_add sym syms' -> if ((sym_ident sym) == ident) then Just (sym, (Sym_empty, syms))
+                               else case (walk syms' ident) of
+                                      Nothing -> Nothing
+                                      Just (cand, (pasts, remains)) -> Just (cand, (Sym_add sym pasts, remains))
+  in
+    case sym_tbl of
+      Scope_empty -> Nothing
+      Scope_add (lv, anon_idents, syms) sym_tbl' -> (case (walk syms ident) of
+                                                       Just (cand, (pasts, remains)) -> Just ((sym_attr cand), (pasts', remains'))
+                                                         where
+                                                           pasts' = Scope_add (lv, anon_idents, pasts) Scope_empty
+                                                           remains' = Scope_add (lv, anon_idents, remains) sym_tbl'
+                                                       Nothing -> (case sym_search' sym_tbl' ident of
+                                                                     Nothing -> Nothing
+                                                                     Just (cand, (pasts, remains)) -> Just (cand, (Scope_add (lv, anon_idents, syms) pasts, remains))
+                                                                  )
+                                                    )
+  )
 
 sym_lkup_tydef_decl :: Symtbl -> String -> Maybe (Sym_attrib, Symtbl)
 sym_lkup_tydef_decl symtbl ident =
@@ -209,6 +232,40 @@ sym_lkup_tydef_decl symtbl ident =
                                  _ -> sym_lkup_tydef_decl symtbl' ident
                               )
     Nothing -> Nothing
+  )
+sym_lkup_tydef_decl' :: Symtbl -> String -> Maybe ((Sym_attrib, (Sym_tbl, Sym_tbl)), Symtbl)
+sym_lkup_tydef_decl' symtbl ident =
+  ras_trace "in sym_lkup_tydef_decl" (
+  case sym_search' (sym_categorize symtbl Sym_cat_typedef) ident of
+    Nothing -> Nothing
+    Just (attr, h@(past, remains)) -> (case sym_attr_entity attr of
+                                         Syn_tydef_decl _ _ -> Just ((attr, h), symtbl)
+                                         _ -> (case sym_lkup_tydef_decl' (sym_update symtbl Sym_cat_typedef remains) ident of
+                                                 Just ((attr, (past', remains')), _) -> Just ((attr, ((join past past'), remains')), symtbl)
+                                                 Nothing -> Nothing
+                                              )
+                                      )
+      where
+        join :: Sym_tbl -> Sym_tbl -> Sym_tbl
+        join tbl1@(Scope_add (lv_1, anon_idents_1, syms_1) tbl1') tbl2@(Scope_add (lv_2, anon_idents_2, syms_2) tbl2') =
+          case tbl1' of
+            Scope_empty ->
+              -- assert ((lv_1 == lv_2) && (anon_idents_1 == anon_idents_2))
+              (case syms_2 of
+                 Sym_empty -> Scope_add (lv_1, anon_idents_1, syms_1) tbl2'
+                 _ -> (case syms_1 of
+                         Sym_empty -> tbl2
+                         _ -> Scope_add (lv_1, anon_idents_1, (cat syms_1 syms_2)) tbl2'
+                      )
+              )
+              where
+                cat :: Symtbl_cluster -> Symtbl_cluster -> Symtbl_cluster
+                cat (Sym_add sym syms1') syms2 =
+                  case syms1' of
+                    Sym_empty -> Sym_add sym syms2
+                    _ -> Sym_add sym (cat syms1' syms2)
+            
+            _ -> Scope_add (lv_1, anon_idents_1, syms_1) (join tbl1' tbl2)
   )
 
 sym_lkup_fun :: Symtbl -> String -> Maybe (Sym_attrib, Symtbl)
